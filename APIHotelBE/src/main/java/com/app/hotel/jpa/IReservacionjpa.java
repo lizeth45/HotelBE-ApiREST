d1ba6e7a3cc570d0a340631217377175504d6285
Lizeth Rodriguez <101153941+lizeth45@users.noreply.github.com>
2023-11-12 18:08:48 -0600

    Delete APIHotelBE directory

--- APIHotelBE/src/main/java/com/app/hotel/jpa/IReservacionjpa.java
+++
@@ -1,8 +0,0 @@
-package com.app.hotel.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.app.hotel.entity.Reservacion;
-
-public interface IReservacionjpa extends JpaRepository<Reservacion, Integer> {
-
-}
